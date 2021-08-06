b37862871e2adfe4e3f0a35b3eb9f16f961f3754
Hariom Chaturvedi <hariom4694@gmail.com>
2021-08-06 18:41:03 +0530

    testing workflow

--- prepare.sh
+++ prepare.sh
@@ -3,7 +3,7 @@
 ls -la ./config/filebeat
 ls -la ./config/filebeat/modules.d/
 
-chown root ./config/filebeat/filebeat.yml ./config/filebeat/modules.d/
+sudo chown 1000:1000 ./config/filebeat/filebeat.yml ./config/filebeat/modules.d/
 
 chmod 644 ./config/filebeat/filebeat.yml
 chmod 644 ./config/filebeat/modules.d/*
